ad691d8426686c609ea4618c0cc4c624ae16b735
河本行平 <me32140941@gmail.com>
2025-07-15 19:41:07 +0900

    ui変更

--- trustshop/resources/views/products/show.blade.php
+++ trustshop/resources/views/products/show.blade.php
@@ -1,6 +1,25 @@
 @extends('layouts.app')
 @section('content')
- @php
-    var_dump($product);
- @endphp
+<div class="text-center">
+    <div class="text-xl mb-4">
+      <h1>商品名</h1>
+      <h2>{{$product->name}}</h2>
+    </div>
+
+    @if($product->description!==null)
+    <div class="text-xl mb-4">
+        <h1>商品説明</h1>
+    </div>
+    <p>{{$product->description}}</p>
+    @endif
+
+    <div class="text-xl mb-4">
+      <h1>在庫</h1>
+    </div>
+    @if($product->stock <= 0)
+      <p>SOLD OUT</p>
+    @else
+      <p>購入</p>
+    @endif
+</div>
 @endsection
--- trustshop/resources/views/shops/show.blade.php
+++ trustshop/resources/views/shops/show.blade.php
@@ -1,24 +1,9 @@
 @extends('layouts.app')
 @section('content')
+<div class="text-center">
     <div class="text-xl mb-4">
         <h1>{{ $shop->user->name }}のショップ</h1>
     </div>
-    <div class="mb-4">
-        ショップ名 <br>
-        {{ $shop->name }}
-    </div>
-    <div class="mb-4">
-        説明 <br>
-        {{ $shop->description }}
-    </div>
-    <div class="text-xl mt-4">
-        <h1>商品一覧</h1>
-    </div>
-
-    @foreach($products as $product)
-      <a href="{{route('products.show', $product->id)}}" class="underline">{{$product->name}}</a>
-    @endforeach
-
     @if (Auth::id() == $shop->user_id)
     <form action="{{ route('shops.destroy', $shop->id) }}" method="POST">
         @csrf
@@ -27,5 +12,30 @@
     </form>
     <a href="{{ route('shops.edit', $shop->id) }}" class="underline">編集</a>
     @endif
+    <div class="mb-4">
+        ショップ名 <br>
+        {{ $shop->name }}
+    </div>
+    <div class="mb-4">
+        説明 <br>
+        {{ $shop->description }}
+    </div>
 
+    <div class="text-xl mt-4">
+        <h1>商品一覧</h1>
+    </div>
+
+    @foreach($products as $product)
+      <a href="{{route('products.show', $product->id)}}" class="underline">{{$product->name}}</a>
+      <p>商品価格</p>
+      <p>{{$product->price}}円</p>
+      <p>在庫</p>
+      @if($product->stock <= 0)
+        <p>SOLD OUT</p>
+      @else
+        <p>{{$product->stock}}個</p>
+      @endif
+
+    @endforeach
+</div>
 @endsection
